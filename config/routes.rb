4dc89256259b90b68fb1ab77a2389fdacf1936dc
VicenteLavagnino <vicente.lavagnino@uc.cl>
2023-09-29 14:18:41 -0300

    make windows

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
                    path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   root 'home#index'
 
-  get '/explora', to: 'explora#index'
-  get '/nosotros', to: 'nosotros#index'
-  get 'trabaja', to: 'trabaja#index'
-  get 'contacto', to: 'contacto#index'
-  get 'reporta', to: 'reporta#index'
+  get '/explora', to: 'home#explora'
+  get '/nosotros', to: 'home#nosotros'
+  get '/trabaja', to: 'home#trabaja'
+  get '/contacto', to: 'home#contacto'
+  get '/reporta', to: 'home#reporta'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
