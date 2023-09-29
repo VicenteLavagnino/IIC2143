aaf3bcabdda5e842251380e2439571a3ae014432
VicenteLavagnino <vicente.lavagnino@uc.cl>
2023-09-29 13:07:47 -0300

    navbar updates (pretry)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }, 
                    path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   root 'home#index'
+
+  get '/explora', to: 'explora#index'
+  get '/nosotros', to: 'nosotros#index'
+  get 'trabaja', to: 'trabaja#index'
+  get 'contacto', to: 'contacto#index'
+  get 'reporta', to: 'reporta#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
