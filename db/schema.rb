282b061dae86b2857098d5f2007a6759dfa82b33
leonelcespedess <leonel.cespedes@uc.cl>
2023-10-28 19:04:39 -0300

    render_test

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema[7.0].define(version: 2023_10_28_183513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,7 +79,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_28_183513) do
     t.datetime "reset_password_sent_at"
     t.text "description"
     t.string "image"
-    t.bigint "number"
+    t.string "phone_number"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -88,10 +89,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_28_183513) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "chat_users", "chats"
-  add_foreign_key "chat_users", "usuarios"
-  add_foreign_key "chats", "requests"
   add_foreign_key "messages", "chats"
   add_foreign_key "messages", "users"
   add_foreign_key "products", "users"
