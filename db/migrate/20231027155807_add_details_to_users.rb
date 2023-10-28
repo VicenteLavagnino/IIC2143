2b59f3e1a8454061d01021ebbf67d0f8abb6bc83
leonelcespedess <leonel.cespedes@uc.cl>
2023-10-28 16:34:47 -0300

    migration_user_fix

--- db/migrate/20231027155807_add_details_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddDetailsToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :description, :string
-    add_column :users, :phone_number, :string
-    add_column :users, :image, :string
-  end
-end
