1a0bcbbe0c45f96add9af4e94189805c5314f0a2
CrisDonosoMaass <106105065+CrisDonosoMaass@users.noreply.github.com>
2023-10-28 21:42:03 -0300

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,4 +6,5 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rails db:drop RAILS_ENV=production
+bundle exec rake db:create
 bundle exec rake db:migrate
