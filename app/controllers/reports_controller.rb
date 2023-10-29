68573dd42914733a0dc516a3f11cb3752cd04a7d
VicenteLavagnino <vicente.lavagnino@uc.cl>
2023-10-28 23:47:00 -0300

    fix rubocop

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,12 +1,12 @@
 class ReportsController < ApplicationController
-    def user_reports
-        @user_reports = UserReport.all
-    end
+  def user_reports
+    @user_reports = UserReport.all
+  end
 
-    def destroy
-        @user_report = UserReport.find(params[:id])
-        @user_report.destroy
-        redirect_to user_reports_path, notice: "Reporte eliminado con éxito"
-    end
+  def destroy
+    @user_report = UserReport.find(params[:id])
+      @user_report.destroy
+      redirect_to user_reports_path, notice: "Reporte eliminado con éxito"
+  end
 
 end
--- app/controllers/user_reports_controller.rb
+++ app/controllers/user_reports_controller.rb
@@ -1,6 +1,6 @@
 class UserReportsController < ApplicationController
-    def destroy
-        @user_report = UserReport.find(params[:id])
-        @user_report.destroy
-    end
+  def destroy
+    @user_report = UserReport.find(params[:id])
+      @user_report.destroy
+  end
 end
